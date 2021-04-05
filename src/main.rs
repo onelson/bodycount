896502d2caf54357c684923dcf7a5d5fddffd7e3
Owen Nelson <onelson@gmail.com>
2021-04-05 16:01:25 -0700

    Text detection via DNN. Requires opencv 4.5.1.
    Seems to work okay with many sample images, but a couple don't seem to
    recognize anything. Might be okay once we get video moving through and
    there are more frames to read from.

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,143 @@
-use opencv::core::{no_array, Scalar};
-use opencv::features2d::DrawMatchesFlags;
-use opencv::imgcodecs::{IMREAD_COLOR, IMREAD_GRAYSCALE};
-use opencv::types::{VectorOfDMatch, VectorOfi8};
-use opencv::{
-    core, features2d, features2d::Feature2DTrait, highgui, imgcodecs, prelude::*, Result,
+//! Port of https://github.com/opencv/opencv/blob/master/samples/dnn/text_detection.cpp
+//! Check the source cpp file for where to get the NN files.
+
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
 };
 
-fn main() -> Result<()> {
-    let mask = no_array()?;
+use opencv::{
+    core::{self, Point2f, Scalar, Size},
+    dnn, highgui, imgcodecs, imgproc,
+    prelude::*,
+    types::{VectorOfPoint2f, VectorOfString, VectorOfVectorOfPoint},
+};
 
-    let target = imgcodecs::imread("you-died-bw.png", IMREAD_GRAYSCALE)?;
+type Result<T, E = Box<dyn Error>> = std::result::Result<T, E>;
+
+fn main() -> Result<()> {
     let full_frame = imgcodecs::imread(
         &std::env::args().nth(1).expect("input image"),
-        IMREAD_GRAYSCALE,
+        imgcodecs::IMREAD_COLOR,
     )?;
 
-    let height = 160;
-    let width = 910;
-    let x = 490;
-    let y = 465;
-
+    // XXX: need to mess with the crop once we get video moving through
+    let height = 480;
+    let width = 1100;
+    let x = 380;
+    let y = 300;
     let frame = Mat::roi(&full_frame, core::Rect::new(x, y, width, height))?;
 
-    let mut orb = features2d::ORB::default()?;
+    // Parameters.
+    let conf_threshold = 0.5;
+    let nms_threshold = 0.4;
 
-    let (kp1, des1) = {
-        let mut kps = opencv::types::VectorOfKeyPoint::new();
-        let mut descriptors = Mat::default()?;
-        orb.detect_and_compute(&target, &mask, &mut kps, &mut descriptors, false)?;
-        (kps, descriptors)
-    };
+    let imread_rgb = false;
 
-    let (kp2, des2) = {
-        let mut kps = opencv::types::VectorOfKeyPoint::new();
-        let mut descriptors = Mat::default()?;
-        orb.detect_and_compute(&frame, &mask, &mut kps, &mut descriptors, false)?;
-        (kps, descriptors)
-    };
+    let det_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("dnn")
+        .join("frozen_east_text_detection.pb");
+    let rec_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("dnn")
+        .join("CRNN_VGG_BiLSTM_CTC.onnx");
+    let voc_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("dnn")
+        .join("alphabet_36.txt");
 
-    let bf = features2d::BFMatcher::new(core::NORM_HAMMING, true)?;
-    let matches = {
-        let mut matches = VectorOfDMatch::default();
-        bf.train_match(&des1, &des2, &mut matches, &mask)?;
-        matches
-    };
+    // Load networks.
+    let mut detector =
+        dnn::TextDetectionModel_EAST::from_file(det_model_path.to_str().unwrap(), "")?;
+    detector
+        .set_confidence_threshold(conf_threshold)?
+        .set_nms_threshold(nms_threshold)?;
+    let mut recognizer =
+        dnn::TextRecognitionModel::from_file(rec_model_path.to_str().unwrap(), "")?;
 
-    println!("matches: {}", matches.len());
+    // Load vocabulary
+    let mut vocabulary = VectorOfString::new();
+    let voc_file = BufReader::new(File::open(voc_path)?);
+    for voc_line in voc_file.lines() {
+        vocabulary.push(&voc_line?);
+    }
+    recognizer
+        .set_vocabulary(&vocabulary)?
+        .set_decode_type("CTC-greedy")?;
 
-    let mut matches = matches.to_vec();
+    // Parameters for Recognition
+    let rec_scale = 1. / 127.5;
+    let rec_mean = Scalar::from((127.5, 127.5, 127.5));
+    let rec_input_size = Size::new(100, 32);
+    recognizer.set_input_params(rec_scale, rec_input_size, rec_mean, false, false)?;
 
-    matches.sort_by_key(|x| x.distance as i64);
+    // Parameters for Detection
+    let det_scale = 1.;
+    let det_input_size = Size::new(320, 320);
+    let det_mean = Scalar::from((123.68, 116.78, 103.94));
+    let swap_rb = true;
+    detector.set_input_params(det_scale, det_input_size, det_mean, swap_rb, false)?;
 
-    for match_ in &matches {
-        println!(
-            "{:?}",
-            (
-                match_.query_idx,
-                match_.img_idx,
-                match_.train_idx,
-                match_.distance
-            )
-        );
+    let win_name = "EAST: An Efficient and Accurate Scene Text Detector";
+
+    println!("Frame size: {:?}", frame.size()?);
+    // Detection
+    let mut det_results = VectorOfVectorOfPoint::new();
+    detector.detect(&frame, &mut det_results)?;
+
+    if !det_results.is_empty() {
+        // Text Recognition
+        let rec_input = if !imread_rgb {
+            let mut rec_input = Mat::default()?;
+            imgproc::cvt_color(&frame, &mut rec_input, imgproc::COLOR_BGR2GRAY, 0)?;
+            Some(rec_input)
+        } else {
+            None
+        };
+
+        for quadrangle in &det_results {
+            let mut quadrangle_2f = VectorOfPoint2f::new();
+            for pt in &quadrangle {
+                quadrangle_2f.push(Point2f::new(pt.x as f32, pt.y as f32))
+            }
+            let cropped = four_points_transform(
+                rec_input.as_ref().unwrap_or(&frame),
+                quadrangle_2f.as_slice(),
+            )?;
+
+            let recognition_result = recognizer.recognize(&cropped)?;
+            println!("Recognition result: {}", recognition_result);
+        }
     }
 
-    let mut out = Mat::default()?;
-    let matches_mask = VectorOfi8::default();
-    features2d::draw_matches(
-        &target,
-        &kp1,
-        &frame,
-        &kp2,
-        &VectorOfDMatch::from_iter(matches.into_iter().take(3)),
-        &mut out,
-        Scalar::new(0., 127., 0., 0.),
-        Scalar::new(0., 0., 127., 0.),
-        &matches_mask,
-        DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS,
-    )?;
-    highgui::imshow("hello opencv!", &out)?;
-    highgui::wait_key(30000)?;
+    highgui::imshow(win_name, &frame)?;
+    highgui::wait_key(10_000)?;
+
     Ok(())
+}
+
+fn four_points_transform(frame: &Mat, vertices: &[Point2f]) -> Result<Mat> {
+    let output_size = Size::new(100, 32);
+    let target_vertices = [
+        Point2f::new(0., (output_size.height - 1) as f32),
+        Point2f::new(0., 0.),
+        Point2f::new((output_size.width - 1) as f32, 0.),
+        Point2f::new(
+            (output_size.width - 1) as f32,
+            (output_size.height - 1) as f32,
+        ),
+    ];
+    let rotation_matrix =
+        imgproc::get_perspective_transform_slice(&vertices, &target_vertices, core::DECOMP_LU)?;
+    let mut out = Mat::default()?;
+    imgproc::warp_perspective(
+        frame,
+        &mut out,
+        &rotation_matrix,
+        output_size,
+        imgproc::INTER_LINEAR,
+        core::BORDER_CONSTANT,
+        Scalar::default(),
+    )?;
+    Ok(out)
 }
