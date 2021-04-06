20ec3b81c9bb747b31c7b3ea8abdc7f3e47861df
Owen Nelson <onelson@gmail.com>
2021-04-05 20:56:19 -0700

    Running the DNN on frames coming in via UDP.

--- src/main.rs
+++ src/main.rs
@@ -10,78 +10,20 @@ use std::{
 
 use opencv::{
     core::{self, Point2f, Scalar, Size},
-    dnn, highgui, imgcodecs, imgproc,
+    dnn, highgui, imgproc,
     prelude::*,
     types::{VectorOfPoint2f, VectorOfString, VectorOfVectorOfPoint},
 };
 
 type Result<T, E = Box<dyn Error>> = std::result::Result<T, E>;
 
-fn main() -> Result<()> {
-    let full_frame = imgcodecs::imread(
-        &std::env::args().nth(1).expect("input image"),
-        imgcodecs::IMREAD_COLOR,
-    )?;
-
-    // XXX: need to mess with the crop once we get video moving through
-    let height = 480;
-    let width = 1100;
-    let x = 380;
-    let y = 300;
-    let frame = Mat::roi(&full_frame, core::Rect::new(x, y, width, height))?;
-
-    // Parameters.
-    let conf_threshold = 0.5;
-    let nms_threshold = 0.4;
-
+fn detect_text(
+    frame: &Mat,
+    detector: &TextDetectionModel_EAST,
+    recognizer: &TextRecognitionModel,
+) -> Result<()> {
     let imread_rgb = false;
 
-    let det_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("dnn")
-        .join("frozen_east_text_detection.pb");
-    let rec_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("dnn")
-        .join("CRNN_VGG_BiLSTM_CTC.onnx");
-    let voc_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("dnn")
-        .join("alphabet_36.txt");
-
-    // Load networks.
-    let mut detector =
-        dnn::TextDetectionModel_EAST::from_file(det_model_path.to_str().unwrap(), "")?;
-    detector
-        .set_confidence_threshold(conf_threshold)?
-        .set_nms_threshold(nms_threshold)?;
-    let mut recognizer =
-        dnn::TextRecognitionModel::from_file(rec_model_path.to_str().unwrap(), "")?;
-
-    // Load vocabulary
-    let mut vocabulary = VectorOfString::new();
-    let voc_file = BufReader::new(File::open(voc_path)?);
-    for voc_line in voc_file.lines() {
-        vocabulary.push(&voc_line?);
-    }
-    recognizer
-        .set_vocabulary(&vocabulary)?
-        .set_decode_type("CTC-greedy")?;
-
-    // Parameters for Recognition
-    let rec_scale = 1. / 127.5;
-    let rec_mean = Scalar::from((127.5, 127.5, 127.5));
-    let rec_input_size = Size::new(100, 32);
-    recognizer.set_input_params(rec_scale, rec_input_size, rec_mean, false, false)?;
-
-    // Parameters for Detection
-    let det_scale = 1.;
-    let det_input_size = Size::new(320, 320);
-    let det_mean = Scalar::from((123.68, 116.78, 103.94));
-    let swap_rb = true;
-    detector.set_input_params(det_scale, det_input_size, det_mean, swap_rb, false)?;
-
-    let win_name = "EAST: An Efficient and Accurate Scene Text Detector";
-
-    println!("Frame size: {:?}", frame.size()?);
-    // Detection
     let mut det_results = VectorOfVectorOfPoint::new();
     detector.detect(&frame, &mut det_results)?;
 
@@ -110,9 +52,6 @@ fn main() -> Result<()> {
         }
     }
 
-    highgui::imshow(win_name, &frame)?;
-    highgui::wait_key(10_000)?;
-
     Ok(())
 }
 
@@ -140,4 +79,87 @@ fn four_points_transform(frame: &Mat, vertices: &[Point2f]) -> Result<Mat> {
         Scalar::default(),
     )?;
     Ok(out)
+}
+
+use opencv::dnn::{TextDetectionModel_EAST, TextRecognitionModel};
+use opencv::videoio;
+
+fn main() -> Result<()> {
+    let conf_threshold = 0.5;
+    let nms_threshold = 0.4;
+
+    let det_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("dnn")
+        .join("frozen_east_text_detection.pb");
+    let rec_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("dnn")
+        .join("CRNN_VGG_BiLSTM_CTC.onnx");
+    let voc_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("dnn")
+        .join("alphabet_36.txt");
+
+    // Load networks.
+    let mut detector =
+        dnn::TextDetectionModel_EAST::from_file(det_model_path.to_str().unwrap(), "")?;
+    detector
+        .set_confidence_threshold(conf_threshold)?
+        .set_nms_threshold(nms_threshold)?;
+    let mut recognizer =
+        dnn::TextRecognitionModel::from_file(rec_model_path.to_str().unwrap(), "")?;
+
+    // Load vocabulary
+    let mut vocabulary = VectorOfString::new();
+    let voc_file = BufReader::new(File::open(voc_path)?);
+    for voc_line in voc_file.lines() {
+        vocabulary.push(&voc_line?);
+    }
+
+    recognizer
+        .set_vocabulary(&vocabulary)?
+        .set_decode_type("CTC-greedy")?;
+
+    // Parameters for Recognition
+    let rec_scale = 1. / 127.5;
+    let rec_mean = Scalar::from((127.5, 127.5, 127.5));
+    let rec_input_size = Size::new(100, 32);
+    recognizer.set_input_params(rec_scale, rec_input_size, rec_mean, false, false)?;
+
+    // Parameters for Detection
+    let det_scale = 1.;
+    let det_input_size = Size::new(320, 320);
+    let det_mean = Scalar::from((123.68, 116.78, 103.94));
+    let swap_rb = true;
+    detector.set_input_params(det_scale, det_input_size, det_mean, swap_rb, false)?;
+
+    let window = "bodycount";
+    highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+
+    let mut cam = videoio::VideoCapture::from_file("udp://@127.0.0.1:9999", videoio::CAP_FFMPEG)?;
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
+    if !opened {
+        panic!("Unable to open default camera!");
+    }
+    let mut i = 0;
+    let mut frame = Mat::default()?;
+
+    loop {
+        i += 1;
+        cam.grab()?;
+        if i % 10 == 0 {
+            i = 0;
+
+            cam.retrieve(&mut frame, 0)?;
+            let size = frame.size()?;
+            if size.width > 0 {
+                detect_text(&frame, &detector, &recognizer)?;
+                highgui::imshow(window, &mut frame)?;
+            }
+        }
+
+        let key = highgui::wait_key(10)?;
+        if key > 0 && key != 255 {
+            break;
+        }
+    }
+    Ok(())
 }
