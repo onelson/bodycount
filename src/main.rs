0a063ee567b19c1dc5117cf74fa2fe856b34823e
Owen Nelson <onelson@gmail.com>
2021-04-17 14:11:44 -0700

    Update for opencv-rust 0.53.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 use opencv::{core, highgui, imgcodecs, imgproc, prelude::*, Result};
 
 fn main() -> Result<()> {
-    let target = imgcodecs::imread("you-died.png", imgcodecs::IMREAD_COLOR)?;
+    let target = imgcodecs::imread(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/", "you-died.png"),
+        imgcodecs::IMREAD_COLOR,
+    )?;
     let full_frame = imgcodecs::imread(
         &std::env::args().nth(1).expect("input image"),
         imgcodecs::IMREAD_COLOR,
     )?;
 
-    let height = 160;
-    let width = 910;
-    let x = 490;
-    let y = 465;
-    let frame = Mat::roi(&full_frame, core::Rect::new(x, y, width, height))?;
+    // FIXME: need to accept offsets for the bbox to match within.
+    // let height = 160;
+    // let width = 910;
+    // let x = 490;
+    // let y = 465;
+    // let frame = Mat::roi(&full_frame, core::Rect::new(x, y, width, height))?;
+
+    let frame = full_frame;
 
     highgui::imshow("frame", &frame)?;
 
-    let mask = core::no_array()?;
+    let mask = Mat::default();
 
-    let mut hsv1 = Mat::default()?;
-    let mut hsv2 = Mat::default()?;
+    let mut hsv1 = Mat::default();
+    let mut hsv2 = Mat::default();
 
     imgproc::cvt_color(&target, &mut hsv1, imgproc::COLOR_BGR2HSV, 0)?;
     imgproc::cvt_color(&frame, &mut hsv2, imgproc::COLOR_BGR2HSV, 0)?;
 
-    let mut hist1 = Mat::default()?;
-    let mut hist2 = Mat::default()?;
+    let mut hist1 = Mat::default();
+    let mut hist2 = Mat::default();
 
     debug_assert_eq!(hsv1.channels()?, 3);
     debug_assert_ne!(hsv1.size()?.width, 0);
@@ -52,8 +58,8 @@ fn main() -> Result<()> {
     )
     .expect("calc hist 2");
 
-    let mut norm1 = Mat::default()?;
-    let mut norm2 = Mat::default()?;
+    let mut norm1 = Mat::default();
+    let mut norm2 = Mat::default();
     core::normalize(&hist1, &mut norm1, 0., 1., core::NORM_MINMAX, -1, &mask)?;
     core::normalize(&hist2, &mut norm2, 0., 1., core::NORM_MINMAX, -1, &mask)?;
 
